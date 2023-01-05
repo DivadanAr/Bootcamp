697739bc96f76a042c8c522a5f9fe3084ad35813
DivadanAr <divadan.arya12@gmail.com>
2023-01-05 08:21:09 +0700

    Bootcamp Day 1

---
+++ script.js
@@ -0,0 +1,15 @@
+var orang1 = {
+  nama: "Ardel Fadila",
+  umur: 16,
+  fansnyaBagas: true,
+  bicara: function(){
+    console.log("hallo")
+  },
+  berdiri: function(){
+    console.log(this.nama + "Sudah berdiri")
+  },
+  duduk: function(){
+    console.log(this.nama + "sudah duduk")
+  }
+}
+
